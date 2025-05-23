1ec99d924c79f30d50b22945d254d603efe5ad5d
kazuhikosatomi <roughcutjewel87@gmail.com>
2025-05-24 00:56:10 +0900

    Update check_userid_profile.py

--- send/check_userid_profile.py
+++ send/check_userid_profile.py
@@ -1,7 +1,7 @@
 import requests
 
 ACCESS_TOKEN = 'BwGrH6y8tZX64RYfQq1RMiBDrtx8AkyzWfw9433F5H/UxA2InnZ+v+ORa1hxa8jTqNL9tbKKYmgEsJZ8b8bF8kGqhNwp1vKd3/kBAzxn0sN/EuM0Mls20zUtQ+CBfFOlUfXkHbUuN64wsUTa9e7/UAdB04t89/1O/w1cDnyilFU='
-USER_ID = 'Ue9ad8031a3ed1611fa0c7c78a84ac45c'
+USER_ID = 'Ue9ad8031a3a16d11fa01c77a98a4c45c'
 
 headers = {
     "Authorization": f"Bearer {ACCESS_TOKEN}"
